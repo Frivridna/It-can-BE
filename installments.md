d9060d5b52c60f1db31a9915ef684478fc1b6ddd
Mir Albertioni <fethullah.dincer@gmail.com>
2021-06-03 18:17:39 +0200

    added code

---
+++ installments.md
@@ -0,0 +1,17 @@
+## Installments
+Npm install
+Mongoose - npm i mongoose
+npm install @babel/helper-compilation-targets --save-dev
+npm update / npm update --legacy-peer-deps
+## Sockets
+npm i socket.io for BE npm i socket.io-client (Client version of the library) for FE
+
+// Flöde x) 2. Big room with 6-10 people --> (A, B) (C, D) (E, F) split 2 and 2 into smaller rooms. --> "Breakout room" The soundstream starts when everybody (every user) has gone into a room for 2.
+
+// Flöde y) create session - väljer hur många mskor som ska vara med i sessionen - join session - skriver koden för rummet. --> LOGIN.
+
+// Flöde o) 2 och 2 endast.
+EN PERSON
+
+// Flöde p) Create session --> Start session
+node server.js
--- server.js
+++ server.js
@@ -1,21 +1,63 @@
+import mongoose from 'mongoose'
+const socketIo = require("socket.io")
 import express from 'express'
-import bodyParser from 'body-parser'
 import cors from 'cors'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/sounds"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.Promise = Promise
+
+const urlSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique:true,
+    required: true
+  },
+  url: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  description: {
+    type: String,
+    trim: true,
+    required: true
+  }
+})
+
+const Sound = mongoose.model('Sound', urlSchema)
+
+const port = process.env.PORT || 4001
 const app = express()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
-app.use(bodyParser.json())
+app.use(express.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.get('/sounds', async (req,res) => {
+  const allSounds = await urlSchema.find()
+  res.json(allSounds)
+});
+
+app.post('/sounds', async (req,res) => {
+  try {
+    const newSound = await new urlSchema({
+      name: req.body.name,
+      url: req.body.url,
+      description: req.body.description
+    }).save(); 
+    res.json(newSound);
+  } catch (error) {
+    if(error.code===11000){
+      res.status(400).json({ error: 'Duplicated value', field: error.keyValue })
+    }
+    res.status(400).json(error)
+  }
 })
 
 // Start the server
